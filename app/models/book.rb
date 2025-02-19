7314552ffaccae39c6ebc00f435d96062ebf3a7f
achdaisy <oduordaizy@gmail.com>
2025-02-19 07:53:54 +0300

    Update: Added a method to check if a book is available

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
   has_many :lending_histories, dependent: :destroy
   validates :title, presence: true
   validates :title, presence: true
+
+  def available? # Checks if book is available
+    lending_histories.where(returned_at: nil).empty?
+  end
 end
