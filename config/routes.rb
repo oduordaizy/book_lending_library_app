f7b6fae4fe5ae61ec4cbb8a8dc7e4bd3e0264401
achdaisy <oduordaizy@gmail.com>
2025-02-19 08:52:33 +0300

    Update: Added member routes for books, and also created a route for lending histories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   get "pages/home"
   root "pages#home"
+
   resources :books
+    member do
+      post "borrow"
+      post "return_book"
+    end
+
+  resources :lending_histories, only: [ :index ]
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
